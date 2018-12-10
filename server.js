5be34a1e9cc1b639920578e6b3a9c41c851a1d3a
SiennaS <ssienna48@gmail.com>
2018-12-09 21:23:06 -0500

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // Dependencies
-// =============================================================
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 var express = require("express");
-var path = require("path");
+// Body-parser allows us to receive our information back in JSON form so it is easy to manipulate
+var bodyParser = require("body-parser");
+
+
 
 // Sets up the Express App
-// =============================================================
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -12,4 +15,19 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+// directing server to "route" files.
+// a how to respond when users visit or request data from different URLs.
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// LISTENER
+// Server to start listening
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
 
